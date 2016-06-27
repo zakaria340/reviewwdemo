53202d3939cdf9916f3a7e94260a5a7e8d91b32d
hp4 <hp4@192.168.1.11>
2016-06-27 00:09:08 +0000

    console

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -3,49 +3,46 @@
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
-class AppKernel extends Kernel
-{
-    public function registerBundles()
-    {
-        $bundles = [
-            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
-                    new Tmdb\SymfonyBundle\TmdbSymfonyBundle()
-        ];
+class AppKernel extends Kernel {
 
-        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
-            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
-            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-        }
+  public function registerBundles() {
+    $bundles = [
+      new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+      new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+      new Symfony\Bundle\TwigBundle\TwigBundle(),
+      new Symfony\Bundle\MonologBundle\MonologBundle(),
+      new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+      new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+      new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+      new AppBundle\AppBundle(),
+      new Tmdb\SymfonyBundle\TmdbSymfonyBundle(),
+      new \Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle()
+    ];
 
-        return $bundles;
+    if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
+      $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
+      $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+      $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+      $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
     }
 
-    public function getRootDir()
-    {
-        return __DIR__;
-    }
+    return $bundles;
+  }
 
-    public function getCacheDir()
-    {
-        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
-    }
+  public function getRootDir() {
+    return __DIR__;
+  }
 
-    public function getLogDir()
-    {
-        return dirname(__DIR__).'/var/logs';
-    }
+  public function getCacheDir() {
+    return dirname(__DIR__) . '/var/cache/' . $this->getEnvironment();
+  }
 
-    public function registerContainerConfiguration(LoaderInterface $loader)
-    {
-        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
-    }
+  public function getLogDir() {
+    return dirname(__DIR__) . '/var/logs';
+  }
+
+  public function registerContainerConfiguration(LoaderInterface $loader) {
+    $loader->load($this->getRootDir() . '/config/config_' . $this->getEnvironment() . '.yml');
+  }
+
 }
